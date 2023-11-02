a833a53b7be932c24d336166ada393de07510ca3
Brendon <brendon_busker1@baylor.edu>
2023-11-01 20:16:52 -0500

    added support for monthly

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
 
     # loop bool
     loop = True
+    yearly = "y"
     
     # Input values
     while loop:
@@ -18,16 +19,24 @@ def main():
             beg_balance = float(input('Enter Beginning Balance Amount: '))
             rate = float(input('Enter Rate (MUST BE DECIMAL FORM): '))
             payment = float(input('Enter Payment (GET FROM TVM CALC): '))
-            year = int(input('Enter Years: '))
+            year = int(input('Enter Years/Months: '))
+            yearly = str(input("Enter 'y' for yearly, enter 'm' for monthly: "))
             loop = False
         
         except:
             print('You entered something wrong. Please start over.\n')
 
     # Runs the calculator
-    calc(beg_balance, rate, payment, year)
+    if yearly == 'y':
+        calc_yearly(beg_balance, rate, payment, year)
+    
+    elif yearly == 'm':
+        calc_monthly(beg_balance, rate, payment, year)
+    
+    else:
+        print("You did not enter 'y' or 'm' for years/months, run program again.")
 
-def calc(beg_balance, rate, payment, year):
+def calc_yearly(beg_balance, rate, payment, year):
     print('-' * 86)
     print('YEAR', '|BEGINNING BALANCE', '|TOTAL PAYMENT', '|INTEREST PAYMENT', '|PRINCIPAL PAYMENT', '|ENDING BALANCE', sep = '')
     print('-' * 86)
@@ -40,6 +49,27 @@ def calc(beg_balance, rate, payment, year):
         end_balance = beg_balance - principal
 
         print(f'{i:<4}', f'{round(beg_balance,2):<17}', f'{round(payment,2):<13}',
+               f'{round(interest,2):<16}', f'{round(principal,2):<17}', round(end_balance,2))
+
+        beg_balance = end_balance
+        total_interest += round(interest,2)
+    
+    print()
+    print(f"Total Interest: {round(total_interest,2)}")
+
+def calc_monthly(beg_balance, rate, payment, year):
+    print('-' * 87)
+    print('MONTH', '|BEGINNING BALANCE', '|TOTAL PAYMENT', '|INTEREST PAYMENT', '|PRINCIPAL PAYMENT', '|ENDING BALANCE', sep = '')
+    print('-' * 87)
+
+    total_interest = 0
+
+    for i in range(1, year+1):
+        interest = (rate/12) * beg_balance
+        principal = payment - interest
+        end_balance = beg_balance - principal
+
+        print(f'{i:<5}', f'{round(beg_balance,2):<17}', f'{round(payment,2):<13}',
                f'{round(interest,2):<16}', f'{round(principal,2):<17}', round(end_balance,2))
 
         beg_balance = end_balance
